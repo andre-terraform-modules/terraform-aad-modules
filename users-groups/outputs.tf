0afc15af154fe3bb43a0c77e639c287ac783214c
andrefilipemm <andre.monteiro@farfetch.com>
2024-09-22 01:10:33 +0100

    minor fix: users-groups module outputs

--- users-groups/outputs.tf
+++ users-groups/outputs.tf
@@ -1,7 +1,7 @@
 output "group_ids" {
-  value = azuread_group.group[*].id
+  value = [for g in values(azuread_group.group) : g.id]
 }
 
 output "user_ids" {
-  value = azuread_user.user[*].id
+  value = [for user in local.users_by_group : azuread_user.user["${user.group}-${user.email}"].id]
 }
